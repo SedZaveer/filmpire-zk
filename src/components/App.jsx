bb63c94406857daaf66ec4438eb3e6ca1cea7502
Zaveer Khan <zaveerkhan.fire@gmail.com>
2023-01-23 01:57:00 +0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Switch, Route } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, Navbar, Profile } from './index';
+import useStyles from './styles';
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path={['/', '/approved']}>
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
